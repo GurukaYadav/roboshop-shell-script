d930e85df7864c63b0ac3ffc25e1c9dd313a26c6
Avinash Yadav <avinash.guruka@gmail.com>
2022-12-07 16:58:10 +0530

    updating roboshop-shell-script

--- components/cart.sh
+++ components/cart.sh
@@ -1,1 +1,18 @@
-echo Install cart component
+curl --silent --location https://rpm.nodesource.com/setup_16.x | sudo bash -
+yum install nodejs -y
+
+useradd roboshop
+
+curl -s -L -o /tmp/cart.zip "https://github.com/roboshop-devops-project/cart/archive/main.zip"
+cd /home/roboshop
+rm -rf cart
+unzip /tmp/cart.zip
+mv cart-main cart
+cd cart
+npm install
+
+mv /home/roboshop/cart/systemd.service /etc/systemd/system/cart.service
+sed -i -e 's/REDIS_ENDPOINT/redis.roboshop.internal/' -e 's/CATALOGUE_ENDPOINT/catalogue.roboshop.internal/' /etc/systemd/system/cart.service
+systemctl daemon-reload
+systemctl restart cart
+systemctl enable cart
