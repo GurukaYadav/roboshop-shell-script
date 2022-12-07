9f6b1cdece93c0ae45cbe5d0ebfc0b0f0e5e3937
Avinash Yadav <avinash.guruka@gmail.com>
2022-12-07 18:05:02 +0530

    updating roboshop-shell-script

--- components/dispatch.sh
+++ components/dispatch.sh
@@ -13,7 +13,7 @@ go build
 
 
 mv /home/roboshop/dispatch/systemd.service /etc/systemd/system/dispatch.service
-#sed -i -e 's/AMQPHOST'
+sed -i -e 's/AMQPHOST/rabbitmq.roboshop.internal/'  /etc/systemd/system/dispatch.service
 systemctl daemon-reload
 systemctl enable dispatch
 systemctl restart dispatch
