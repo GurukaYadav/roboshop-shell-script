2aa0ff655341e65dd97bd6ef1d0df2b5468dd8bc
Avinash Yadav <avinash.guruka@gmail.com>
2022-12-07 19:25:45 +0530

    updating roboshop-shell-script

--- components/mysql.sh
+++ components/mysql.sh
@@ -7,7 +7,7 @@ systemctl start mysqld
 
 MYSQL_DEFAULT_PASSWORD=$(grep 'temporary password' /var/log/mysqld.log | awk '{print $NF}')
 
-echo "ALTER USER 'root'@'localhost' IDENTIFIED BY "${MYSQL_PASSWORD}";"|mysql --connect-expired-password -uroot -p"${MYSQL_DEFAULT_PASSWORD}"
+echo "ALTER USER 'root'@'localhost' IDENTIFIED BY "${MYSQL_PASSWORD}"|mysql --connect-expired-password -uroot -p"${MYSQL_DEFAULT_PASSWORD}"
 echo "uninstall plugin validate_password;" | mysql -uroot -p"{MYSQL_PASSWORD}"
 
 
@@ -17,4 +17,4 @@ curl -s -L -o /tmp/mysql.zip "https://github.com/roboshop-devops-project/mysql/a
 cd /tmp
 unzip -o mysql.zip
 cd mysql-main
-mysql -u root -p"${MYSQL_PASSWORD}"<shipping.sql
+mysql -u root -p"${MYSQL_PASSWORD}" <shipping.sq
