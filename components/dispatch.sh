5e19fed6807078cda5f900e2fb1a0638257edea1
Avinash Yadav <avinash.guruka@gmail.com>
2022-12-07 18:01:17 +0530

    updating roboshop-shell-script

--- components/dispatch.sh
+++ components/dispatch.sh
@@ -1,1 +1,19 @@
-echo Install dispatch component
+yum install golang -y
+
+useradd roboshop
+
+curl -L -s -o /tmp/dispatch.zip https://github.com/roboshop-devops-project/dispatch/archive/refs/heads/main.zip
+unzip -o /tmp/dispatch.zip
+mv dispatch-main dispatch
+cd dispatch
+go mod init dispatch
+go get
+go build
+
+
+
+mv /home/roboshop/dispatch/systemd.service /etc/systemd/system/dispatch.service
+#sed -i -e 's/AMQPHOST'
+# systemctl daemon-reload
+# systemctl enable dispatch
+# systemctl start dispatch
