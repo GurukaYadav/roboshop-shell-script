3c2b9be9a0209bc075e506c0807f99a0c898d6e9
Avinash Yadav <avinash.guruka@gmail.com>
2022-12-08 18:03:14 +0530

    updating roboshop-shell-script

--- components/common.sh
+++ components/common.sh
@@ -1,6 +1,6 @@
 CHECK_ROOT() {
 USER_ID=$(id -u)
-if [$USER_ID -ne 0]; then
+if [ $USER_ID -ne 0 ]; then
 	echo You are Non Root user
 	echo You can run this script with Root user or with sudo
 	exit 1
