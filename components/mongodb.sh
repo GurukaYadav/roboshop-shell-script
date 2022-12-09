1e153695bd1a65b8c85ca78f87e59da3006698e1
Avinash Yadav <avinash.guruka@gmail.com>
2022-12-09 21:07:50 +0530

    updating roboshop-shell-script

--- components/mongodb.sh
+++ components/mongodb.sh
@@ -2,20 +2,34 @@ source components/common.sh
 
 CHECK_ROOT
 
-curl -s -o /etc/yum.repos.d/mongodb.repo https://raw.githubusercontent.com/roboshop-devops-project/mongodb/main/mongo.repo
+PRINT "Download mongodb repo"
+curl -s -o /etc/yum.repos.d/mongodb.repo https://raw.githubusercontent.com/roboshop-devops-project/mongodb/main/mongo.repo &>>${LOG}
+CHECK_STAT $?
 
-yum install -y mongodb-org
-systemctl enable mongod
-systemctl start mongod
+PRINT "Install mongodb"
+yum install -y mongodb-org &>>${LOG}
+CHECK_STAT $?
 
-sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/mongod.conf
-systemctl restart mongod
+PRINT "Update mongod configuration"
+sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/mongod.conf &>>${LOG}
+CHECK_STAT $?
+
+PRINT " Start mongodb service"
+systemctl enable mongod &>>${LOG} && systemctl restart mongod &>>${LOG}
+CHECK_STAT $?
 
 
-curl -s -L -o /tmp/mongodb.zip "https://github.com/roboshop-devops-project/mongodb/archive/main.zip"
+PRINT "Download schema"
+curl -s -L -o /tmp/mongodb.zip "https://github.com/roboshop-devops-project/mongodb/archive/main.zip" &>>${LOG}
+CHECK_STAT $?
+
 cd /tmp
-unzip -o mongodb.zip
-cd mongodb-main
-mongo < catalogue.js
-mongo < users.js
+
+PRINT "Extracting mongodb content"
+unzip -o mongodb.zip &>>${LOG}
+CHECK_STAT $?
+
+PRINT "Load mongodb content"
+cd mongodb-main && mongo < catalogue.js &>>${LOG} &&  mongo < users.js &>>${LOG}
+CHECK_STAT $?
 
