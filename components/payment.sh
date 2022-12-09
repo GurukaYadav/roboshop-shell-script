40bbf2219360291ff183293ca9b0daea009d89a8
Avinash Yadav <avinash.guruka@gmail.com>
2022-12-09 19:33:48 +0530

    updating roboshop-shell-script

--- components/payment.sh
+++ components/payment.sh
@@ -2,28 +2,50 @@ source components/common.sh
 
 CHECK_ROOT
 
-yum install python36 gcc python3-devel -y
+PRINT "Install python 3"
+yum install python36 gcc python3-devel -y &>>${LOG}
+CHECK_STAT $?
 
-useradd roboshop
+PRINT "Create application user"
+ id roboshop
+ if [ $? -ne 0 ]; then
+	useradd roboshop &>>${LOG}
+ fi
+ CHECK_STAT $?
 
 cd /home/roboshop
-rm -rf payment
-curl -L -s -o /tmp/payment.zip "https://github.com/roboshop-devops-project/payment/archive/main.zip"
-unzip /tmp/payment.zip
+
+PRINT "Remove old payment content"
+rm -rf payment &>>${LOG}
+CHECK_STAT $?
+
+PRINT "Download payment content"
+curl -L -s -o /tmp/payment.zip "https://github.com/roboshop-devops-project/payment/archive/main.zip" &>>${LOG}
+CHECK_STAT $?
+
+PRINT "Extract payment content"
+unzip /tmp/payment.zip &>>${LOG}
+CHECK_STAT $?
+
 mv payment-main payment
-
-
-cd /home/roboshop/payment
-pip3 install -r requirements.txt
-
+PRINT "Install python dependencies"
+cd /home/roboshop/payment && pip3 install -r requirements.txt &>>${LOG}
+CHECK_STAT $?
 
 USER_ID=$(id -u roboshop)
 GROUP_ID=$(id -g roboshop)
+
+PRINT "Update user and group id's"
 sed -i -e '/^uid/ c /uid=${USER_ID}/'  -e '/^gid/ c /gid=${GROUP_ID}/' /home/roboshop/payment/payment.ini
+CHECK_STAT $?
 
+PRINT "Update systemd configuration"
+mv /home/roboshop/payment/systemd.service /etc/systemd/system/payment.service &>>${LOG}
+CHECK_STAT $?
 
-mv /home/roboshop/payment/systemd.service /etc/systemd/system/payment.service
-sed -i -e 's/CARTHOST/cart.roboshop.internal/'  -e 's/USERHOST/user.roboshop.internal/' -e 's/AMQPHOST/rabbitmq.roboshop.internal/'  /etc/systemd/system/payment.service
-systemctl daemon-reload
-systemctl enable payment
-systemctl restart payment
+PRINT "Setup systemd configuration"
+sed -i -e 's/CARTHOST/cart.roboshop.internal/'  -e 's/USERHOST/user.roboshop.internal/' -e 's/AMQPHOST/rabbitmq.roboshop.internal/'  /etc/systemd/system/payment.service &>>${LOG}
+CHECK_STAT $?
+
+PRINT "Start payment service"
+systemctl daemon-reload && systemctl enable payment &>>${LOG} && systemctl restart payment &>>${LOG}
