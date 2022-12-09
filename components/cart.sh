7bf0f855bd660d53324686772306448829364eba
Avinash Yadav <avinash.guruka@gmail.com>
2022-12-09 12:48:06 +0530

    updating roboshop-shell-script

--- components/cart.sh
+++ components/cart.sh
@@ -1,51 +1,20 @@
-source components/common.sh
+curl --silent --location https://rpm.nodesource.com/setup_16.x | sudo bash -
+yum install nodejs -y
 
-CHECK_ROOT
+useradd roboshop
 
-PRINT "Install NodeJS repos"
-curl --silent --location https://rpm.nodesource.com/setup_16.x | sudo bash &>>${LOG}
-CHECK_STAT $?
-
-PRINT "Install NodeJS"
-yum install nodejs -y &>>${LOG}
-CHECK_STAT $?
-
-PRINT "Creating application user"
-useradd roboshop &>>${LOG}
-CHECK_STAT $?
-
-PRINT "Downloading cart content"
-curl -s -L -o /tmp/cart.zip "https://github.com/roboshop-devops-project/cart/archive/main.zip" &>>${LOG}
-CHECK_STAT $?
-
+curl -s -L -o /tmp/cart.zip "https://github.com/roboshop-devops-project/cart/archive/main.zip"
 cd /home/roboshop
-
-PRINT "Removing previous cart content"
-rm -rf cart &>>${LOG}
-CHECK_STAT $?
-
-PRINT "Extracting cart content"
-unzip /tmp/cart.zip &>>${LOG}
-CHECK_STAT $?
-
+rm -rf cart
+unzip /tmp/cart.zip
 mv cart-main cart
 cd cart
+npm install
 
-PRINT "Install NodeJS dependencies"
-npm install &>>${LOG}
-CHECK_STAT $?
+sed -i -e 's/REDIS_ENDPOINT/redis.roboshop.internal/'  -e 's/CATALOGUE_ENDPOINT/catalogue.roboshop.internal/'  /home/roboshop/cart/systemd.service
 
-PRINT "Setup SystemD configuration"
-mv /home/roboshop/cart/systemd.service /etc/systemd/system/cart.service &>>${LOG}
-CHECK_STAT $?
-
-PRINT "Update systemd configuration"
-sed -i -e 's/REDIS_ENDPOINT/redis.roboshop.internal/' -e 's/CATALOGUE_ENDPOINT/catalogue.roboshop.internal/' /etc/systemd/system/cart.service &>>${LOG}
-CHECK_STAT $?
-
+mv /home/roboshop/cart/systemd.service /etc/systemd/system/cart.service
 systemctl daemon-reload
+systemctl start cart
 systemctl enable cart
 
-PRINT "Start cart service"
-systemctl restart cart &>>${LOG}
-CHECK_STAT $?
