4f7d5ae1951c23b6c7c043e3f7da95b95535bd10
Avinash Yadav <avinash.guruka@gmail.com>
2022-12-09 20:52:42 +0530

    updating roboshop-shell-script

--- components/common.sh
+++ components/common.sh
@@ -158,3 +158,42 @@ MAVEN() {
 
  SYSTEMD
 }
+
+GOLANG() {
+ CHECK_ROOT
+
+ PRINT "Install golang"
+ yum install golang -y &>>${LOG}
+ CHECK_STAT $?
+
+ PRINT "Create application user"
+ id roboshop
+ if [ $? -ne 0 ]; then
+   useradd roboshop &>>${LOG}
+ fi
+ CHECK_STAT $?
+
+
+ PRINT "Download ${COMPONENT} content"
+ curl -L -s -o /tmp/${COMPONENT}.zip https://github.com/roboshop-devops-project/${COMPONENT}/archive/refs/heads/main.zip &>>${LOG}
+ CHECK_STAT $?
+
+ cd /home/roboshop
+
+ PRINT "Remove old ${COMPONENT} content"
+ rm -rf ${COMPONENT} &>>${LOG}
+ CHECK_STAT $?
+
+ PRINT "Extract ${COMPONENT} content"
+ unzip  /tmp/${COMPONENT}.zip &>>${LOG}
+ CHECK_STAT $?
+
+ mv dispatch-main ${COMPONENT}
+ cd ${COMPONENT}
+
+ PRINT "Install golang dependencies"
+ go mod init ${COMPONENT} &>>${LOG} && go get &>>${LOG} && go build &>>${LOG}
+ CHECK_STAT $?
+
+ SYSTEMD
+}
--- components/dispatch.sh
+++ components/dispatch.sh
@@ -2,22 +2,47 @@ source components/common.sh
 
 CHECK_ROOT
 
-yum install golang -y
+PRINT "Install golang"
+yum install golang -y &>>${LOG}
+CHECK_STAT $?
 
-useradd roboshop
+PRINT "Create application user"
+id roboshop
+if [ $? -ne 0 ]; then
+  useradd roboshop &>>${LOG}
+fi
+CHECK_STAT $?
 
-curl -L -s -o /tmp/dispatch.zip https://github.com/roboshop-devops-project/dispatch/archive/refs/heads/main.zip
+
+PRINT "Download ${COMPONENT} content"
+curl -L -s -o /tmp/${COMPONENT}.zip https://github.com/roboshop-devops-project/${COMPONENT}/archive/refs/heads/main.zip &>>${LOG}
+CHECK_STAT $?
+
 cd /home/roboshop
-rm -rf dispatch
-unzip  /tmp/dispatch.zip
-mv dispatch-main dispatch
-cd dispatch
-go mod init dispatch
-go get
-go build
 
-mv /home/roboshop/dispatch/systemd.service  /etc/systemd/system/dispatch.service
-sed -i -e 's/AMQPHOST/rabbitmq.roboshop.internal/'  /etc/systemd/system/dispatch.service
-systemctl daemon-reload
-systemctl enable dispatch
-systemctl restart dispatch
+PRINT "Remove old ${COMPONENT} content"
+rm -rf ${COMPONENT} &>>${LOG}
+CHECK_STAT $?
+
+PRINT "Extract ${COMPONENT} content"
+unzip  /tmp/${COMPONENT}.zip &>>${LOG}
+CHECK_STAT $?
+
+mv dispatch-main ${COMPONENT}
+cd ${COMPONENT}
+
+PRINT "Install golang dependencies"
+go mod init ${COMPONENT} &>>${LOG} && go get &>>${LOG} && go build &>>${LOG}
+CHECK_STAT $?
+
+PRINT "Update systemd configuration"
+mv /home/roboshop/${COMPONENT}/systemd.service  /etc/systemd/system/${COMPONENT}.service &>>${LOG}
+CHECK_STAT $?
+
+PRINT "Setup systemd configuration"
+sed -i -e 's/AMQPHOST/rabbitmq.roboshop.internal/'  /etc/systemd/system/${COMPONENT}.service &>>${LOG}
+CHECK_STAT $?
+
+PRINT "Start ${COMPONENT} service"
+systemctl daemon-reload && systemctl enable ${COMPONENT} &>>${LOG} && systemctl restart ${COMPONENT} &>>${LOG}
+CHECK_STAT $?
