640a213913008f8e34ff17bd8e764e7e342ae596
Avinash Yadav <avinash.guruka@gmail.com>
2022-12-09 15:50:22 +0530

    updating roboshop-shell-script

--- components/cart.sh
+++ components/cart.sh
@@ -1,55 +1,5 @@
 source components/common.sh
 
-CHECK_ROOT
-
-PRINT "Setting up NodeJS repository"
-curl --silent --location https://rpm.nodesource.com/setup_16.x | sudo bash &>>${LOG}
-CHECK_STAT $?
-
-PRINT "Install NodeJS"
-yum install nodejs -y &>>${LOG}
-CHECK_STAT $?
-
-PRINT "Create application user"
-id roboshop
-if [ $? -ne 0 ]; then
-	useradd roboshop &>>${LOG}
-fi
-CHECK_STAT $?
-
-PRINT "Download cart content"
-curl -s -L -o /tmp/cart.zip "https://github.com/roboshop-devops-project/cart/archive/main.zip" &>>${LOG}
-CHECK_STAT $?
-
-cd /home/roboshop &>>${LOG}
-
-PRINT "Remove old cart content"
-rm -rf cart &>>${LOG}
-CHECK_STAT $?
-
-PRINT "Extract cart content"
-unzip /tmp/cart.zip &>>${LOG}
-CHECK_STAT $?
-
-mv cart-main cart
-cd cart
-
-PRINT "Install NodeJS dependencies"
-npm install &>>${LOG}
-CHECK_STAT $?
-
-PRINT "Update systemD configuration"
-sed -i -e 's/REDIS_ENDPOINT/redis.roboshop.internal/'  -e 's/CATALOGUE_ENDPOINT/catalogue.roboshop.internal/'  /home/roboshop/cart/systemd.service &>>${LOG}
-CHECK_STAT $?
-
-PRINT "Setup systemD configuration"
-mv /home/roboshop/cart/systemd.service /etc/systemd/system/cart.service &>>${LOG}
-CHECK_STAT $?
-
-systemctl daemon-reload
-systemctl enable cart
-
-PRINT "Start cart service"
-systemctl restart cart &>>${LOG}
-CHECK_STAT $?
+COMPONENT=cart
+NODEJS
 
--- components/common.sh
+++ components/common.sh
@@ -24,3 +24,58 @@ PRINT() {
  echo "-------------$1------------" &>>${LOG}
  echo "$1"
 }
+
+NODEJS() {
+ CHECK_ROOT
+
+ PRINT "Setting up NodeJS repository"
+ curl --silent --location https://rpm.nodesource.com/setup_16.x | sudo bash &>>${LOG}
+ CHECK_STAT $?
+
+ PRINT "Install NodeJS"
+ yum install nodejs -y &>>${LOG}
+ CHECK_STAT $?
+
+ PRINT "Create application user"
+ id roboshop
+ if [ $? -ne 0 ]; then
+	useradd roboshop &>>${LOG}
+ fi
+ CHECK_STAT $?
+
+ PRINT "Download ${COMPONENT} content"
+ curl -s -L -o /tmp/${COMPONENT}.zip "https://github.com/roboshop-devops-project/${COMPONENT}/archive/main.zip" &>>${LOG}
+ CHECK_STAT $?
+
+ cd /home/roboshop &>>${LOG}
+
+ PRINT "Remove old ${COMPONENT} content"
+ rm -rf ${COMPONENT} &>>${LOG}
+ CHECK_STAT $?
+
+ PRINT "Extract ${COMPONENT} content"
+ unzip /tmp/${COMPONENT}.zip &>>${LOG}
+ CHECK_STAT $?
+
+ mv ${COMPONENT}-main ${COMPONENT}
+ cd ${COMPONENT}
+
+ PRINT "Install NodeJS dependencies"
+ npm install &>>${LOG}
+ CHECK_STAT $?
+
+ PRINT "Update systemD configuration"
+ sed -i -e 's/REDIS_ENDPOINT/redis.roboshop.internal/'  -e 's/CATALOGUE_ENDPOINT/catalogue.roboshop.internal/'  /home/roboshop/${COMPONENT}/systemd.service &>>${LOG}
+ CHECK_STAT $?
+
+ PRINT "Setup systemD configuration"
+ mv /home/roboshop/${COMPONENT}/systemd.service /etc/systemd/system/${COMPONENT}.service &>>${LOG}
+ CHECK_STAT $?
+
+ systemctl daemon-reload
+ systemctl enable ${COMPONENT}
+
+ PRINT "Start ${COMPONENT} service"
+ systemctl restart ${COMPONENT} &>>${LOG}
+ CHECK_STAT $?
+}
